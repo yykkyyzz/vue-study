6deb729a73cc604aeb8d20c4459021f4501c527c
Yuko Yuza <>
2022-04-21 18:43:01 +0900

    13回用ソースアップ

---
+++ src/13/App.vue
@@ -0,0 +1,56 @@
+<!-- "Standard" way of doing things -->
+<template>
+  <div>
+    w: <CounterPanel
+      :counter="widthCounter"
+      @change-count="changeWidth"
+    />
+    </div>
+    <div>
+    h: <CounterPanel
+      :counter="heightCounter"
+      @change-count="changeHeight"
+    />
+    </div>
+    <div>
+      <button @click="clearCount">clear</button>
+    </div>
+    <hr/>
+    <img src="./360192black.png" :width="widthCounter.count" :height="heightCounter.count"/>
+</template>
+
+<script>
+
+import CounterPanel from '../components/13/CounterPanel.vue';
+import Counter from '../assets/13/js/Counter.js';
+
+export default {
+  components: {
+    CounterPanel,
+  },
+  data() {
+    const options = {
+      min: 50,
+      max: 1000,
+      step: 50,
+    };
+    return {
+      widthCounter: new Counter(200, options),
+      heightCounter: new Counter(150, options),
+    };
+  },
+  methods: {
+    changeWidth(width) {
+      this.widthCounter.count = width;
+    },
+    changeHeight(height) {
+      this.heightCounter.count = height;
+    },
+    clearCount() {
+      this.widthCounter.count = this.widthCounter.initialCount;
+      this.heightCounter.count = this.heightCounter.initialCount;
+    },
+  }
+}
+
+</script>
---
+++ src/components/13/CounterPanel.vue
@@ -0,0 +1,53 @@
+<template>
+    <button @click="countDown">-</button>
+    <input v-model.number.lazy="countTmp" type="number" :min="counter.min" :max="counter.max"/>
+    <button @click="countUp">+</button>
+</template>
+
+<script>
+import Counter from '../../assets/13/js/Counter.js';
+
+export default {
+  
+  props: {
+    counter: {
+      type: Counter,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      countTmp: 0, // v-model用のデータ、とりあえず適当な値をセット
+    };
+  },
+  emits: ['changeCount'],
+  watch: {
+    // propsのcounterをウォッチする
+    counter: {
+      handler(counter) {
+        // コンポーネントの初期化時、counterデータが親側で変更された時に実行される
+        this.countTmp = counter.count;
+
+        // computedと違い、副作用が必要な場合は記述可能
+      },
+      deep: true, // オブジェクトの持つプロパティ値が変化した時も実行
+      immediate: true, // 初期化時も実行
+    },
+    // v-modelからの入力があった場合に実行される
+    countTmp(newCount) {
+      this.$emit('changeCount', newCount);
+
+      // computedと違い、副作用が必要な場合は記述可能
+    },
+  },
+  methods: {
+    countDown() {
+      this.$emit('changeCount', this.counter.stepDownValue);
+    },
+    countUp() {
+      this.$emit('changeCount', this.counter.stepUpValue);
+    },
+  },
+}
+</script>
+
